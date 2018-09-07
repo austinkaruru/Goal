63e8e1926a7ccbebcee930a15db3523c0004dbea
Austin Karuru <karuruaustine@gmail.com>
2018-09-07 12:18:24 +0300

    initial commit

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from flask import render_template
+from app import app
+
+# Views
+
+
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = "Goals pitching"
+    return render_template('index.html', message=message)
